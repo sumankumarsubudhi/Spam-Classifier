a2f7e0d1cf1b8918439c8f7ce3dbaa239e13dcdc
Suman Kumar Subudhi <sonu61866@gmail.com>
2019-07-08 18:47:47 +0530

    Update spam_classifier.py

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,9 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing the dataset into a pandas dataframe
 df = pd.read_table('SMSSpamCollection', sep = '\t', header = None, names = ['labels','sms_message']
                
-# Converting our labels to binary variables for ease of computation
+# Converting the labels to binary variables for ease of computation
 df['label'] = df.label.map({'ham':0, 'spam':1})
 
+# Splitting the dataset into a training and testing set by using the train_test_split method in sklearn                   
+X_train, X_test, y_train, y_test = train_test_split(df['sms_message'], df['label'])
+                   
+# Instantiating the CountVectorizer method
+count_vector = CountVectorizer()
+
+# Fitting the training data and then return the matrix
+training_data = count_vector.fit_transform(X_train)
+
+# Transforming testing data and return the matrix
+testing_data = count_vector.transform(X_test)                   
